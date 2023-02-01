795c35ea9d803a7a9b52dc3fe41ef910dd36a0d9
Hamsa Pillai <hamsa@Hamsas-Mac.attlocal.net>
2023-01-31 16:56:44 -0800

    Added GPT wrapper and summarization prompt

---
+++ gpt_wrapper.py
@@ -0,0 +1,15 @@
+import os
+import openai
+
+def send_completion_request(prompt, model_choice="text_davinci-003",
+                            model_temperature=0.7, max_tokens=250):
+    openai.api_key = os.environ.get('OPENAI_API_KEY')
+    response = openai.Completion.create(
+        model=model_choice,
+        prompt=prompt,
+        temperature=model_temperature,
+        max_tokens=max_tokens
+    )
+    return response['choices'][0]['text']
+
+
