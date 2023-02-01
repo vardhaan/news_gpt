38c62f19327849dd93ae14e8ce652161c68d7b00
Hamsa Pillai <hamsa@hamsas-mac.lan>
2023-02-01 12:55:22 -0800

    Minor refactoring. Added tokenizer function

--- classes/News.py
+++ classes/News.py
@@ -7,7 +7,11 @@ class News:
         self.description = description
         self.url = url
         self.published_at = published_at
+        self.summary = None
 
     def __str__(self):
         published_at_string = convert_datetime_to_utc_string(self.published_at)
         return 'Title: ' + self.title + 'Published At: ' + published_at_string
+    
+    def add_summary(self, summary):
+        self.summary = summary
--- gpt_wrapper.py
+++ gpt_wrapper.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-def send_completion_request(prompt, model_choice="text_davinci-003",
+def send_completion_request(prompt, model_choice="text-davinci-003",
                             model_temperature=0.7, max_tokens=250):
     openai.api_key = os.environ.get('OPENAI_API_KEY')
     response = openai.Completion.create(
